d27841e134272de8d7df37eb2068fec055ef7640
Robbert Martens <robbertmartens@gmail.com>
2023-12-03 14:29:44 +0100

    init commit

---
+++ Services/Models/Wishlist.cs
@@ -0,0 +1,10 @@
+﻿namespace Services.Models
+{
+    public class Wishlist
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Id { get; set; }
+        public bool WillBeBought { get; set; }
+    }
+}
---
+++ Services/VerlanglijstjeService.cs
@@ -0,0 +1,10 @@
+﻿namespace Services
+{
+    public class VerlanglijstjeService
+    {
+        public void UpdateWishLists()
+        {
+
+        }
+    }
+}
